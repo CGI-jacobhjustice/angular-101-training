7f32298af99448c70b9e6b872c460e977aca2d63
Jacob Justice <jacob.justice@cgi.com>
2020-03-13 09:15:07 -0500

    add in details component

---
+++ src/app/features/Details/details.component.ts
@@ -0,0 +1,11 @@
+import { Component, Input, OnInit } from '@angular/core';
+import { GenericAssetDetailComponent } from '../_Shared/GenericAssetDetailComponent';
+
+@Component({
+  selector: 'app-details',
+  templateUrl: `./details.component.html`,
+})
+export class DetailsComponent extends GenericAssetDetailComponent  {
+
+}
+
